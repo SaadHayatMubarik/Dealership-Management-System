69f492b901afd23580323d3c1380677f2ec5255b
salman rasheed <salmanrasheed.srk99@yahoo.com>
2024-01-15 15:42:09 +0500

    add employee

--- frontend/src/app/modules/cdms/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/cdms/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  constructor(private router: Router) { }
+
 
 }
