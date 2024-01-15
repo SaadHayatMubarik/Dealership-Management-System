8cfdd20cb916ac7001f21aabe07b4aa2972a0560
salman rasheed <salmanrasheed.srk99@yahoo.com>
2024-01-15 20:33:01 +0500

    search in market

--- frontend/src/app/modules/cdms/search-market/search-market.component.ts
+++ frontend/src/app/modules/cdms/search-market/search-market.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from 'src/app/shared/base.component';
+import { ApiHelperService } from 'src/app/shared/services/api-helper.service';
 
 import {
   DataTableColumn,
   IDataTableAction,
   IObject,
 } from 'src/app/shared/interfaces/common';
+import { IVehicleDetails } from '../../interfaces/inventory';
 
 @Component({
   selector: 'app-search-market',
@@ -14,41 +16,65 @@ import {
 })
 export class SearchMarketComponent extends BaseComponent implements OnInit{
 
+  constructor(private apiService: ApiHelperService){
+    super()
+  }
+
 
   columns: DataTableColumn[] = [];
   actions: IDataTableAction[] = [];
   data: IObject[] = [];
+  vehicleDetails: any[] = [];
+  showroomId = localStorage.getItem("Showroom Id");
+  
 
 
   ngOnInit() {
+    this.getVehicleDetails();
 
-    this.columns = [
-      {
-        field: '',
-        fieldTitle: 'Showroom Name',
-      },
-      {
-        field: '',
-        fieldTitle: 'Showroom State',
-      },
-      {
-        field: '',
-        fieldTitle: 'Showroom City',
-      },
-      {
-        field: '',
-        fieldTitle: 'Showroom Address',
-      },
+    // this.columns = [
+    //   {
+    //     field: '',
+    //     fieldTitle: 'Showroom Name',
+    //   },
+    //   {
+    //     field: '',
+    //     fieldTitle: 'Showroom State',
+    //   },
+    //   {
+    //     field: '',
+    //     fieldTitle: 'Showroom City',
+    //   },
+    //   {
+    //     field: '',
+    //     fieldTitle: 'Showroom Address',
+    //   },
       
-    ];
-    this.actions = [
+    // ];
+    // this.actions = [
      
-      {
-        label: 'View',
-        icon: 'pi pi-file-view',
-        command: () => {},
-      },
-    ];
+    //   {
+    //     label: 'View',
+    //     icon: 'pi pi-file-view',
+    //     command: () => {},
+    //   },
+    // ];
   }
 
-}
+
+  getVehicleDetails() {
+    this.apiService
+    .get(`/inventory/getMarketInventory/${this.showroomId}`)
+    .subscribe((attributes: IVehicleDetails[]) => {
+      this.vehicleDetails = attributes;
+      attributes.forEach((vehicleDetails: IVehicleDetails) => {
+        console.log(vehicleDetails.vehicleMake);
+      });
+    });
+  } 
+
+  
+
+  }
+
+
