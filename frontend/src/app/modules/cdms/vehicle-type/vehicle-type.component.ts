ee89140e597dbb88963c37c208b3de713e42ba37
salman rasheed <salmanrasheed.srk99@yahoo.com>
2024-01-30 00:05:17 +0500

    login link added to signUp

--- frontend/src/app/modules/cdms/vehicle-type/vehicle-type.component.ts
+++ frontend/src/app/modules/cdms/vehicle-type/vehicle-type.component.ts
@@ -116,16 +116,11 @@ export class VehicleTypeComponent extends BaseComponent implements OnInit {
   }
 
   updateVehicleType(){
-    this.apiService.put(`/vehicle-type/updateVehicleType/${this.updatedName}/${this.selectedVehicleTypeId}`).subscribe(
+    this.apiService.patch(`/vehicle-type/updateVehicleType/${this.updatedName}/${this.selectedVehicleTypeId}`).subscribe(
        (response) => {
         this.toast.showSuccess('Updated Successfully');
         this.updateSidebarVisible =false;
         console.log(response);
-      },
-      (error) => {
-        this.toast.showError();
-        this.updateSidebarVisible =false;
-        console.log(error);
       }
     )
   }
