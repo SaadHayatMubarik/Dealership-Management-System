af051c0abd21af0ca0642d8aa78908e5b0a4c74b
unknown <saadyaya5@gmail.com>
2024-03-05 18:14:36 +0500

    customer get ui working

--- backend/src/modules/customer/customer.controller.ts
+++ backend/src/modules/customer/customer.controller.ts
@@ -2,14 +2,16 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerType } from './customer-type.enum';
 import { Customer } from './entity/Customer';
+import { CustomerCatagory } from './customer-catagory.enum';
 
 @Controller('customer')
 export class CustomerController {
     constructor(private customerService: CustomerService){}
 
-    @Get('getCustomer/:showroomId/:customerType')
-    getCustomer(@Param('showroomId') showroomId: number, @Param('customerType') customerType:CustomerType): Promise<Customer[]>{
-        return this.customerService.getCustomer(showroomId,customerType)
+    @Get('getCustomer/:showroomId/:customerCategory')
+    getCustomer(@Param('showroomId') showroomId: number, @Param('customerCategory') CustomerCatagory:CustomerCatagory): Promise<Customer[]>{
+        // console.log(showroomId, customerType)
+        return this.customerService.getCustomer(showroomId,CustomerCatagory)
     }
 
     @Get('getCustomerDetails/:customerId')
--- backend/src/modules/customer/customer.service.ts
+++ backend/src/modules/customer/customer.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Customer } from './entity/Customer';
 import { Repository } from 'typeorm';
 import { CustomerType } from './customer-type.enum';
+import { CustomerCatagory } from './customer-catagory.enum';
 
 @Injectable()
 export class CustomerService {
@@ -11,8 +12,8 @@ export class CustomerService {
         private customerRepository: Repository<Customer>
     ){}
 
-    async getCustomer(showroomId: number, customerType:CustomerType):  Promise<Customer[]>{
-        return await this.customerRepository.findBy({showroom:{showroom_id:showroomId}, type:customerType});
+    async getCustomer(showroomId: number, CustomerCatagory:CustomerCatagory):  Promise<Customer[]>{
+        return await this.customerRepository.findBy({showroom:{showroom_id:showroomId}, catagory:CustomerCatagory});
     }
 
     async getCustomerDetails(customerId: number): Promise<Customer>{
