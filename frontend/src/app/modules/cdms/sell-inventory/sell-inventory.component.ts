e02476beaaadc0096735c01cd2f19262522ce404
salman7420 <salmanrasheed.srk99@yahoo.com>
2024-03-16 22:42:14 +0500

    sell inventory form UI

--- frontend/src/app/modules/cdms/sell-inventory/sell-inventory.component.ts
+++ frontend/src/app/modules/cdms/sell-inventory/sell-inventory.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/shared/base.component';
+import { NgForm } from '@angular/forms';
 
+
+import {
+  DataTableColumn,
+  IDataTableAction,
+  IObject,
+} from 'src/app/shared/interfaces/common';
 @Component({
   selector: 'app-sell-inventory',
   templateUrl: './sell-inventory.component.html',
   styleUrls: ['./sell-inventory.component.scss']
 })
-export class SellInventoryComponent {
+export class SellInventoryComponent extends BaseComponent implements OnInit{
+
+  activeTabIndex = 0;
+  ngOnInit(){
+
+  }
 
 }
