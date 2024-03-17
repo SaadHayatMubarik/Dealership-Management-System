ac31c56aba8bc4fdcef15d8de226bc2e7aaa05d5
salman7420 <salmanrasheed.srk99@yahoo.com>
2024-03-17 17:17:16 +0500

    dynamic form of sell inventory done

--- frontend/src/app/modules/cdms/sell-inventory/sell-inventory.component.ts
+++ frontend/src/app/modules/cdms/sell-inventory/sell-inventory.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from 'src/app/shared/base.component';
 import { NgForm } from '@angular/forms';
+import { ApiHelperService } from 'src/app/shared/services/api-helper.service';
+import { ActivatedRoute } from '@angular/router';
+// import { DatePipe } from '@angular/common';
+
 
 
 import {
@@ -15,9 +19,137 @@ import {
 })
 export class SellInventoryComponent extends BaseComponent implements OnInit{
 
-  activeTabIndex = 0;
-  ngOnInit(){
+
+  data: IObject[] = [];
+  inventoryId : number = 0;
+  vehicleDetails: any = '';
+  showroomId = localStorage.getItem("Showroom Id");
+  activeTabIndex = 0; // Track current active tab index
+  showSecondTab = false; // Initially disable the second tab
+  customerType: string = 'BUYER'
+  BuyerCategory: string[] = ["CUSTOMER", "AGENT", "DEALERSHIP"];
+  SelectedCategory:string = '';
+  customers: any[] = []
+  buyerId: number = 0;
+
+
+  constructor(private apiService:ApiHelperService, private route: ActivatedRoute){
+    super();
+  }
+
+
+  ngOnInit()
+  {
+
+    this.route.params.subscribe(params => {
+      this.inventoryId = params['inventoryId']; 
+      this.getvehicleDetail();
+    
+    });
+
 
   }
+
+  dateOfPurchase: Date | null = null;
+
+  vehicleType : string = ''
+  make : string = '' ;
+  model : string = '' ;
+  variant : string = '' ;
+  chasisNo : string = '' ;
+  engineNo : string = '' ;
+  costPrice : string = '' ;
+  demand : string = '' ;
+  // dateOfPurchase : string = '' ;
+  modelYear : string = '' ;
+  bodyColor : string = '' ;
+  status : string = '' ;
+  regNo : string = '' ;
+  selectedCustomer:any = '';
+
+ 
+
+
+
+  getvehicleDetail(){
+    this.apiService
+  .get(`/inventory/getInventoryDetails/${this.inventoryId}`)
+  .subscribe(
+    (data) => {
+      this.vehicleDetails = data;
+      console.log(data)
+
+      // this.dateOfPurchase = this.datePipe.transform(this.vehicleDetails.date_of_purchase, 'yyyy-MM-dd');
+      this.make = this.vehicleDetails.make
+      this.model = this.vehicleDetails.model
+      this.variant = this.vehicleDetails.variant
+      this.chasisNo = this.vehicleDetails.chasis_no
+      this.engineNo = this.vehicleDetails.engine_no
+      this.costPrice = this.vehicleDetails.price
+      // this.dateOfPurchase = this.vehicleDetails.date_of_purchase
+      this.dateOfPurchase = new Date(this.vehicleDetails.date_of_purchase);
+      this.modelYear = this.vehicleDetails.year
+      this.bodyColor = this.vehicleDetails.color
+      this.status = this.vehicleDetails.status
+      this.regNo = this.vehicleDetails.reg_no
+      this.demand = this.vehicleDetails.demand
+
+ 
+    }
+  );
+  }
+
+
+  getCustomersByType() {
+    this.apiService
+    .get(`/customer/getCustomer/${this.showroomId}/${this. SelectedCategory}/${this.customerType}`)
+    .subscribe({
+      next: (response) => {
+        console.log(response);
+        this.customers = response;
+       
+      },
+      complete: () => {
+      }
+    })
+}
+
+onCustomerSelectionChange(): void {
+  if (this.selectedCustomer) {
+      // Assuming the customer object has an 'id' property
+     this.buyerId=  this.selectedCustomer.customer_id;
+      console.log(this.buyerId);
+
+  }
+  this.getCustomersById();  
+}
+
+
+customersDetails: any;
+phone_no : string = '' ;
+email : string = '' ;
+city : string = '' ;
+address : string = '' ;
+
+
+getCustomersById(){
+  this.apiService
+  .get(`/customer/getCustomerDetails/${this.buyerId}`)
+  .subscribe({
+    next: (response: IObject[]) => {
+      console.log(response);
+      this.customersDetails = response;
+      this.phone_no = this.customersDetails.phoneNo;
+      this.email = this.customersDetails.email;
+      this.city = this.customersDetails.city;
+      this.address = this.customersDetails.address;
+    },
+    error: () => {
+      // this.toast.showError('Server Error!');
+    }
+  })
+}
+
+
 
 }
--- frontend/src/app/modules/cdms/view-detailed-inventory/view-detailed-inventory.component.ts
+++ frontend/src/app/modules/cdms/view-detailed-inventory/view-detailed-inventory.component.ts
@@ -46,12 +46,6 @@ images: any[] = [
   .subscribe(
     (data) => {
       this.vehicleDetails = data;
-// <<<<<<< HEAD
-//       //  this.disableButton();
-// =======
-//       console.log(this.vehicleDetails);
-//       console.log(this.vehicleDetails.demand);
-// >>>>>>> adf8b96a2e878a0bd51a8d37865c8a07df19cd3a
     }
   );
   }
