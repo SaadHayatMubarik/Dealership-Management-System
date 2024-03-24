0444b9d13b7f6030956cccaad7efe617749b53aa
salman7420 <salmanrasheed.srk99@yahoo.com>
2024-03-24 15:22:32 +0500

    update changes

--- frontend/src/app/modules/cdms/update-inventory/update-inventory.component.ts
+++ frontend/src/app/modules/cdms/update-inventory/update-inventory.component.ts
@@ -13,6 +13,7 @@ import { ISellInventory } from '../../interfaces/inventory';
 import {
   IObject
 } from 'src/app/shared/interfaces/common';
+import { IUpdateInventory } from '../../interfaces/update';
 
 
 @Component({
@@ -40,7 +41,8 @@ export class UpdateInventoryComponent extends BaseComponent implements OnInit {
   {
     this.route.params.subscribe(params => {
       this.inventoryId = params['inventoryId']; 
-      this.getvehicleDetail();
+      // this.getvehicleDetail();
+      this.getVehicleDetails(this.inventoryId);
       console.log(this.inventoryId);
     
     });
@@ -49,57 +51,87 @@ export class UpdateInventoryComponent extends BaseComponent implements OnInit {
 
       @ViewChild('vehicleUpdate') vehicleUpdate!: NgForm ;
 
-    vehicleDetails: any = '';
-    make : string = '' ;
-    model : string = '' ;
-    variant : string = '' ;
-    chasisNo : string = '' ;
-    engineNo : string = '' ;
-    costPrice : string = '' ;
-    demand : string = '' ;
-    modelYear : string = '' ;
-    bodyColor : string = '' ;
-    status : string = '' ;
-    regNo : string = '' ;
-    dateOfPurchase: Date | null = null;
- 
-    getvehicleDetail(){
-      this.apiService
-    .get(`/inventory/getInventoryDetails/${this.inventoryId}`)
-    .subscribe(
-      (data) => {
-        this.vehicleDetails = data;
-        console.log('vehicle details object', this.vehicleDetails);
-      
-        this.make = this.vehicleDetails.make
-        this.model = this.vehicleDetails.model
-        this.variant = this.vehicleDetails.variant
-        this.chasisNo = this.vehicleDetails.chasis_no
-        this.engineNo = this.vehicleDetails.engine_no
-        this.costPrice = this.vehicleDetails.price
-        this.dateOfPurchase = new Date(this.vehicleDetails.date_of_purchase);
-        this.modelYear = this.vehicleDetails.year
-        this.bodyColor = this.vehicleDetails.color
-        this.status = this.vehicleDetails.status
-        this.regNo = this.vehicleDetails.reg_no
-        this.demand = this.vehicleDetails.demand
-  
+      updateInventory: IUpdateInventory = 
+      {
+      make : '',
+      model:'' ,
+      variant: '',  
+      chasis_no : '' ,
+      engine_no : '',
+      price : '',
+      demand : '',
+      year : '',
+      color : '',
+      status : ''  ,
+      reg_no : ''  ,
+      date_of_purchase:'',
+
       }
-    );
+
+    
+   
+    // model : string = '' ;
+    // variant : string = '' ;
+    // chasisNo : string = '' ;
+    // engineNo : string = '' ;
+    // costPrice : string = '' ;
+    // demand : string = '' ;
+    // modelYear : string = '' ;
+    // bodyColor : string = '' ;
+    // status : string = '' ;
+    // regNo : string = '' ;
+    // dateOfPurchase: Date | null = null;
+ 
+    // getvehicleDetail(){
+    //   this.apiService
+    // .get(`/inventory/getInventoryDetails/${this.inventoryId}`)
+    // .subscribe(
+    //   (data) => {
+    //     this.vehicleDetails = data;
+    //     console.log('vehicle details object', this.vehicleDetails);
+      
+    //     this.make = this.vehicleDetails.make
+    //     this.model = this.vehicleDetails.model
+    //     this.variant = this.vehicleDetails.variant
+    //     this.chasisNo = this.vehicleDetails.chasis_no
+    //     this.engineNo = this.vehicleDetails.engine_no
+    //     this.costPrice = this.vehicleDetails.price
+    //     this.dateOfPurchase = new Date(this.vehicleDetails.date_of_purchase);
+    //     this.modelYear = this.vehicleDetails.year
+    //     this.bodyColor = this.vehicleDetails.color
+    //     this.status = this.vehicleDetails.status
+    //     this.regNo = this.vehicleDetails.reg_no
+    //     this.demand = this.vehicleDetails.demand
+  
+    //   }
+    // );
+    // }
+
+    getVehicleDetails(inventoryId: number){
+
+      this.apiService
+      .get(`/inventory/getInventoryDetails/${inventoryId}`)
+      .subscribe(
+        (data: IUpdateInventory) => {
+          this.updateInventory = data;
+     
+        }
+      );
+
     }
 
 
     update(){
-
-
-      this.apiService.patch('/inventory/updateInventory/sellInventory', ).subscribe(
+      this.apiService.put('/inventory/updateInventory/sellInventory', this.updateInventory).subscribe(
         next => {
         this.toast.showSuccess('Updated Successfully');
-        
+        console.log('success', next);
+        console.log('object on success', this.updateInventory);
               },
       error => {
-       
       this.toast.showError('Server Error! Please try again later.');
+      console.log('Fail:', error);
+      console.log('object on fail', this.updateInventory);
       },
     )
 
