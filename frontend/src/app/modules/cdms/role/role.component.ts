4605f8430a775176b6a09b8877c343e191e41e51
salman7420 <salmanrasheed.srk99@yahoo.com>
2024-06-09 17:26:34 +0500

    update and delete role done

--- frontend/src/app/modules/cdms/role/role.component.ts
+++ frontend/src/app/modules/cdms/role/role.component.ts
@@ -21,10 +21,13 @@ import { NgForm } from '@angular/forms';
 
 export class RoleComponent extends BaseComponent implements OnInit {
   roleName: string = '';
+  updateRoleName :string = ''
   columns: DataTableColumn[] = [];
   actions: IDataTableAction[] = [];
   data: IObject[] = [];
   components: any[] = [];
+  roleId:number = 0;
+  display : boolean = false;
 
   constructor(
     private readonly apiService: ApiHelperService,
@@ -47,9 +50,32 @@ export class RoleComponent extends BaseComponent implements OnInit {
         fieldTitle: 'Role Name',
       },
       ];
+
+      this.actions = [
+        {
+          label: 'Delete',
+          icon: 'pi pi-trash',
+          command: (event) => {
+            this.roleId = event.role_id;
+            this.deleteRole(this.roleId);
+          },
+          permission: 'delete.roles'
+        },
+        {
+          label: 'Update',
+          icon: 'pi pi-pencil',
+          command: (event) => {
+            this.roleId = event.role_id;
+            this.display = true;
+            
+          },
+          permission: 'update.roles'
+        },
+      ];
   }
 
   @ViewChild('roleForm') roleForm!: NgForm;
+  @ViewChild('updatedRoleForm') roleUpdateForm!: NgForm;
  
   
   modules: { name: string }[] = [];
@@ -70,53 +96,6 @@ export class RoleComponent extends BaseComponent implements OnInit {
       });
     });
   }
-
-
-
-  // saveRolePermissions() {
-  //   const rolePermissionsData: RolePermission[] = [];
-  //   const roleName = this.roleName; // Store role name once
-   
-    // Object.keys(this.rolePermissions).forEach(moduleName => {
-    //   const permissions: ModulePermission = this.rolePermissions[moduleName];
-    //   const rolePermissionData: RolePermission = {
-    //     roleName: roleName,
-    //     component_id: this.components.find(component => component.component.component_name === moduleName)?.component.component_id,
-    //     permissions: permissions,
-    //   };
-    //   rolePermissionsData.push(rolePermissionData);
-    // });
-
-    
-  // Object.keys(this.rolePermissions).forEach(moduleName => {
-  //   const permissions: ModulePermission = this.rolePermissions[moduleName];
-  //   const component_id = this.components.find(component => component.component.component_name === moduleName)?.component.component_id;
-  //   if (component_id !== undefined) { // Ensure component_id is found
-  //     const rolePermissionData: RolePermission = {
-  //       roleName: roleName, // Include role name once
-  //       component_id: component_id,
-  //       permissions: permissions,
-  //     };
-  //     rolePermissionsData.push(rolePermissionData);
-  //   }
-  // });
-
-  //   this.apiService
-  //     .post('/role-based/saveRolePermissions', rolePermissionsData)
-  //     .subscribe(
-  //       (response) => {
-  //         console.log('Role permissions saved successfully:', response);
-  //         console.log('OBJ',rolePermissionsData)
-  //         this.toast.showSuccess('Role permissions saved successfully');
-  //       },
-  //       (error) => {
-  //         console.error('Error saving role permissions:', error);
-  //         console.log('OBJ',rolePermissionsData)
-  //         this.toast.showError('Error saving role permissions');
-  //       }
-  //     );
-  // }
-
   showroom_id = localStorage.getItem('Showroom Id');
 
   saveRolePermissions() {
@@ -155,11 +134,47 @@ export class RoleComponent extends BaseComponent implements OnInit {
   }
 
 
+  
 
   getRoles(){
     this.apiService.get(`/role-based/getRole/${this.showroom_id}`).subscribe((data) => {
       this.data = data;
     });
 }
+
+deleteRole(roleId:number){
+  this.apiService.delete(`/role-based/deleteRole/${roleId}`).subscribe({
+    next: (response) => {
+      this.getRoles();
+      this.toast.showSuccess(`${this.roleName} record deleted.`);
+    },
+    error: () => {
+      this.toast.showError('System Error.');
+    },
+    
+  });
+}
+
+updateRole(){
+  if (this.roleUpdateForm.valid){
+    this.apiService.patch(`/role-based/updateRole/${this.roleId}/${this.updateRoleName}`).subscribe({
+      next: (response) => {
+        this.getRoles();
+        this.toast.showSuccess(`Role Name Update.`);
+        this.display = false;
+        this.roleUpdateForm.reset();
+
+      },
+      error: () => {
+        this.toast.showError('System Error');
+      },
+    });
+  }
+  else{
+    this.toast.showError('Please fill the field correctly')
+  }
+
+}
+
 
 }
